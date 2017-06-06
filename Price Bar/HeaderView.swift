d14f5c8eed4d53808da10efe86b8d8e83ba8fb15
leo mac <leonif1@gmail.com>
2017-06-06 16:07:54 +0300

    init commit

---
+++ Price Bar/HeaderView.swift
@@ -0,0 +1,19 @@
+//
+//  HeaderView.swift
+//  Price Bar
+//
+//  Created by Leonid Nifantyev on 6/5/17.
+//  Copyright © 2017 LionLife. All rights reserved.
+//
+
+import UIKit
+
+class HeaderView: UIView {
+
+    @IBOutlet weak var imageHeader: UIImageView!
+    @IBOutlet weak var categoryLabel: UILabel!
+    
+    
+    
+    
+}
